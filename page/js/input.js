71bf69160393f16d5afc8c825ff98917788ce3ee
taraskami <taras.kami@hotmail.com>
2018-10-19 23:29:09 -0400

    updated css

---
+++ page/js/input.js
@@ -0,0 +1,8 @@
+function input() {
+    var symptoms = Number(document.getElementById("symptoms").value);
+
+    if (symptoms != null) {
+        //Link to Karth's code for finding diagnosis.
+    }
+
+}
