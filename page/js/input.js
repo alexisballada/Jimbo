8664acaaf2cae9da50f703b7aa50922cc015d208
taraskami <taras.kami@hotmail.com>
2018-10-20 03:06:25 -0400

    jquery works a bit better now

--- page/js/input.js
+++ page/js/input.js
@@ -1,8 +1,27 @@
 function input() {
-    var symptoms = Number(document.getElementById("symptoms").value);
+    input = document.getElementById("symptoms").value;
+    alert(input);
 
-    if (symptoms != null) {
-        //Link to Karth's code for finding diagnosis.
+    //find out how to stick this input to karth's python code.
+}
+
+$(document).ready(function(){
+    var colors = ["FFD4D3", "8AD2A2", "FE8783"];
+
+    $("#symptoms").bind("enterKey", function(e){
+        input();
+        var index = Math.floor(Math.random() * 4)
+        $( "#wrapper" ).animate({
+            backgroundColor: "#" + colors[index]
+        }, 1000);
+
+    });
+    $("#symptoms").keyup(function(e){
+        if(e.keyCode == 13) {
+            $(this).trigger("enterKey");
+        }
     }
 
-}
+    
+
+)});
